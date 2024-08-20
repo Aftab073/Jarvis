e4fde297366148fb82409e68eccb6e28e0eb9b33
Aftab073 <tamboliaftab84@gmail.com>
2024-08-20 20:27:08 +0530

    Initial commit with project files

---
+++ music_library.py
@@ -0,0 +1,6 @@
+music = {
+    "Safar": "https://www.youtube.com/watch?v=x42dH5K_Lj0",
+    "Ishq": "https://www.youtube.com/watch?v=IJeSR-LJBnk",
+    "Falling": "https://www.youtube.com/watch?v=eIc4mqyN1Q8"
+
+}
